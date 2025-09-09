8be90a70006e7a47fc3a93609d819e915282bb25
Ojomo Oluwalademi Adedamola <ojomo.l.o9@gmail.com>
2025-09-09 18:34:55 +0100

    update

--- components/ViewAllBar.tsx
+++ components/ViewAllBar.tsx
@@ -1,22 +1,33 @@
 "use client";
 
 import React from "react";
+import Link from "next/link";
 
 type Props = {
-  justify: string;
+  justify?: string; // made optional, defaults to flex-end
   inputText: string;
   href?: string;
 };
 
-const ViewAllBar = ({ justify, inputText, href }: Props) => {
+const ViewAllBar = ({
+  justify = "justify-end",
+  inputText,
+  href = "#",
+}: Props) => {
   return (
     <div
       className={`${justify} flex w-full items-end border-t border-[#797979] bg-[#e4e2e2] px-[60px] py-3`}
       style={{ alignSelf: "stretch" }}
     >
-      <div className="text-lg font-semibold text-[#2c86fc]">
-        <a href={href}>{inputText} --&gt;</a>
-      </div>
+      <Link
+        href={href}
+        className="group text-lg font-semibold text-[#2c86fc] transition-colors hover:text-[#1a5fcc]"
+      >
+        {inputText}
+        <span className="ml-1 inline-block transition-transform group-hover:translate-x-2">
+          →
+        </span>
+      </Link>
     </div>
   );
 };
